1aecf2f175a4f1c38c297bc06ba2e2dff06f7c2b
Antonio Rabuzin <arabuzin@wescale.com>
2023-11-06 17:10:53 +0100

    📱 Fix bg images

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        'night-background': '#11121E',
-        'night-text': '#C6ACA8',
-        "night-heading-text-green": '#68EA5D'
+        "night-background": "#11121E",
+        "night-text": "#C6ACA8",
+        "night-heading-text-green": "#68EA5D"
       },
       fontFamily: {
-        'display': ['Inter'],
-        'body': ['Inter'],
+        display: ["Inter"],
+        body: ["Inter"],
+      },
+      backgroundImage: {
+        avatar: "url('/images/avatar.jpeg')",
+        "banner-day-sm":"url('/images/banner-day-sm.jpg')",
+        "banner-night-sm":"url('/images/banner-night-sm.jpg')",
+        "banner-day-md":"url('/images/banner-day-md.jpg')",
+        "banner-night-md":"url('/images/banner-night-md.jpg')",
+        "banner-day-lg":"url('/images/banner-day-lg.jpg')",
+        "banner-night-lg":"url('/images/banner-night-lg.jpg')",
       }
     },
   },
